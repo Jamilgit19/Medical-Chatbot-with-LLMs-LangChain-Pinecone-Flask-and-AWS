da378c9af2585f319b07f1cb7452b9410c4b20fc
Jamilgit19 <157163338+Jamilgit19@users.noreply.github.com>
2026-05-15 23:12:19 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="MD.Jamil",
+    author_email="jamil.rahman.008@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
